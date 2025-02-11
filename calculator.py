0b2fd4cf14fb3cee0459bf507aa72debf871514c
Kunter <3075044C@student.gla.ac.uk>
2025-02-11 12:00:29 +0000

    The base code for calculator

---
+++ calculator.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+
+class Calculator:
+    
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Calculator App")
+        self.root.geometry("400x500")
+        
+        def addition(self):
+            pass
+        
+        def subtraction(self):
+            pass
+        
+        def multiplication(self):
+            pass
+        
+        def division(self):
+            pass
+        
+        def sine(self):
+            pass
+        
+        def cosine(self):
+            pass
+        
+        def tangent(self):
+            pass
+        
+        def arcsine(self):
+            pass
+        
+        def arccos(self):
+            pass
+        
+        def arctan(self):
+            pass
+        
+        def square(self):
+            pass
+        
+        def squareroot(self):
+            pass
+        
+        def get_result(self):
+            pass
+        
+app = Calculator()
+app.root.mainloop()
